4f470eda40139d7f7745bd89ccccfbb793aebd75
Ankit <sam.megh0305@gmail.com>
2025-05-14 00:02:40 +0530

    animated front screen is done but discription is not for now

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import './App.css'
 import { HomeScreen } from './screens/HomeScreen'
 function App() {
- return <div><HomeScreen/></div>
+ return <div><HomeScreen/>
+ 
+ <div className='h-[100vh]'>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, repellendus cum voluptatem, ad culpa totam vero fugit eveniet doloribus minus ipsa illo eaque magnam? Obcaecati aliquam odit veritatis aut commodi!</p>
+ </div>
+ </div>
 }
 
 export default App
--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,9 +1,45 @@
-import React from "react";
-export function HomeScreen(){
-    return <>
-    <div className="bg-red-500">
-<h1>Hello World!</h1>
-    </div>
+import MyImage from '../assets/Profile_Photo.png';
+import { useEffect, useState } from 'react';
 
-    </>
+export function HomeScreen() {
+  const [scrollY,setScrollY]=useState(0);
+  useEffect(()=>{
+    const handleScroll=()=>setScrollY(window.scrollY);
+    window.addEventListener('scroll',handleScroll); 
+    return ()=> window.removeEventListener('scroll',handleScroll);
+  },[]);
+  const imgMoveX=Math.min(scrollY*0.07,25);
+  const imgBorder= Math.min(scrollY*0.15,50);
+  const imgbackgroundopacity= Math.min(scrollY*0.005,1);
+  const txtMoveX=Math.min(scrollY*0.05,20);
+  const txtSize=Math.max(1-(scrollY*0.005),0.2)
+  return (
+    <div className='relative h-[150vh] w-full'>
+    <div className="sticky top-0 h-screen flex items-center justify-center overflow-hidden">
+      <img
+        className="w-[20vw] relative z-10 object-contain"
+        style={
+          {
+            borderRadius: `${imgBorder}%`,
+            backgroundColor:`rgba(225,225,225,${imgbackgroundopacity})`,
+            transform:`translateX(${-imgMoveX}vw)`,
+            transition: "transform 0.2s ease-out"
+        }
+        }
+        src={MyImage}
+        alt="Profile picture of Sam"
+      />
+      <p className="absolute select-none text-[20vw] z-0 font-bold text-[#00FFFF]"
+      style={
+        {
+          transform:`translateX(${txtMoveX}vw) scale(${txtSize}`,
+            transition:"transform 0.2s ease-out"
+        }
+      }
+      >
+        SAM
+      </p>
+    </div>
+    </div>
+  );
 }
