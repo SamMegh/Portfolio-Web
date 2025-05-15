30b2c0ad5bdac9dad819585c88e8f744d98b1786
Ankit <sam.megh0305@gmail.com>
2025-05-15 23:24:43 +0530

    component are saprated

---
+++ src/components/Skill.Component.jsx
@@ -0,0 +1,10 @@
+import { useEffect, useState, useRef } from "react";
+
+const skillData = {
+  'Web Development': ['React', 'MEAN', 'MERN'],
+  'Android / Mobile': ['Flutter', 'React Native', 'Android Studio'],
+  'Cyber Security': [],
+  Other: ['Git', 'AI', 'Websites', 'Bug Hunter'],
+};
+
+export default Si
