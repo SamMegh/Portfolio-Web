81a03da3cdd28755761797e0a56879b933773b39
Ankit <sam.megh0305@gmail.com>
2025-09-04 19:35:55 +0530

    project horizontal scroll is added

--- src/components/projecttab.jsx
+++ src/components/projecttab.jsx
@@ -3,146 +3,108 @@ import { useEffect } from "react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
-const horiz = document.querySelector("#AllProjects");
 
 export default function ProjectTab() {
+  // All project details in a single variable
+  const projects = [
+    {
+      name: "Project 1",
+      services: "React + Vite",
+      color: "bg-red-300",
+    },
+    {
+      name: "Project 2",
+      services: "React + Vite",
+      color: "bg-red-300",
+    },
+    {
+      name: "Project 3",
+      services: "React + Vite",
+      color: "bg-red-300",
+    },
+    {
+      name: "Project 4",
+      services: "React + Vite",
+      color: "bg-red-300",
+    },
+    {
+      name: "Project 5",
+      services: "React + Vite",
+      color: "bg-red-300",
+    },
+    {
+      name: "Project 6",
+      services: "React + Vite",
+      color: "bg-red-300",
+    },
+    {
+      name: "Project 7",
+      services: "React + Vite",
+      color: "bg-red-300",
+    },
+  ];
+
   useEffect(() => {
-    gsap.timeline({ ease: "power1.easeInOut" }).fromTo(
+    ScrollTrigger.getAll().forEach((st) => st.kill());
+
+    gsap.fromTo(
       ".Project-name",
       { x: -500 },
       {
-        duration: 3.5,
+        duration: 2.5,
         ease: "back.out(2)",
         x: 0,
         scrollTrigger: {
-          trigger: "#pageBlock",
-          scroller: "body",
+          trigger: "#ProjectsSection",
           scrub: 5,
           end: "top 40%",
         },
       }
     );
-    gsap.to("#AllProjects .project", {
-      transform: "translateX(-150%)",
+
+    gsap.to("#AllProjects", {
+      xPercent: -600,
+      ease: "none",
       scrollTrigger: {
-        trigger: "#AllProjects",
-        scroller: "body",
-        markers: true,
-        start: "top 0%",
-        end: "top -200%",
+        trigger: "#ProjectsSection",
+        start: "top top",
+        end: "+=4000",
+        scrub: true,
         pin: true,
-        scrub: 2,
+        anticipatePin: 1,
+        markers: true,
       },
     });
   }, []);
+
   return (
     <div>
-      <div className="AllContent">
-        <div id="pageBlock" className=" ml-10 text-[var(--light)]">
-          <p className="Project-name relative text-[8vw] ">Projects</p>
-        </div>
-        <div
-          id="AllProjects "
-          className="AllProjects h-screen m-15 flex flex-row gap-10"
-        >
-          <div className="p-5 project bg-green-500 w-full md:w-[700px] min-h-[250px] max-h-[50vh] flex flex-col md:flex-row items-center justify-center gap-5">
-            {/* Red Box */}
-            <div className="w-[90vw] h-[100vh]  md:w-[400px] md:h-[300px] bg-red-300 max-w-[500px]"></div>
+      <div
+        id="ProjectsSection"
+        className="h-screen flex flex-col justify-center"
+      >
+        <p className="Project-name text-[8vw] font-bold text-white m-5">
+          Projects
+        </p>
 
-            {/* Text */}
-            <div className="flex-1 max-w-[500px] text-white">
-              <h3 className="title font-bold uppercase text-2xl">
-                this is sam's project
-              </h3>
-              <h5 className="font-normal">
-                Services: <span className="font-semibold">React + Vite</span>
-              </h5>
+        <div id="AllProjects" className="flex gap-10 h-full items-center p-5">
+          {projects.map((proj, i) => (
+            <div
+              //  w-[100vw] sm:w-[50vw] lg:w-[30vw] h-[30vh]
+              className="project flex flex-row p-4 items-center justify-center gap-5 bg-green-500 rounded-lg flex-none w-[550px] h-[350px] max-w-full "
+              key={i}
+            >
+              <div
+                className={`red-box w-[300px] h-[200px] ${proj.color}`}
+              ></div>
+              <div className="text text-white max-w-[400px]">
+                <h3 className="title text-lg font-semibold">{proj.name}</h3>
+                <h5 className="text-sm">
+                  Services: <span className="font-medium">{proj.services}</span>
+                </h5>
+              </div>
             </div>
-          </div>
-          <div className="p-5 bg-green-500 project w-full md:w-[700px] min-h-[250px] max-h-[50vh] flex flex-col md:flex-row items-center justify-center gap-5">
-            {/* Red Box */}
-            <div className="w-[90vw] h-[100vh]  md:w-[400px] md:h-[300px] bg-red-300 max-w-[500px]"></div>
-
-            {/* Text */}
-            <div className="flex-1 max-w-[500px] text-white">
-              <h3 className="title font-bold uppercase text-2xl">
-                this is sam's project
-              </h3>
-              <h5 className="font-normal">
-                Services: <span className="font-semibold">React + Vite</span>
-              </h5>
-            </div>
-          </div>
-          <div className="p-5 bg-green-500 project w-full md:w-[700px] min-h-[250px] max-h-[50vh] flex flex-col md:flex-row items-center justify-center gap-5">
-            {/* Red Box */}
-            <div className="w-[90vw] h-[100vh]  md:w-[400px] md:h-[300px] bg-red-300 max-w-[500px]"></div>
-
-            {/* Text */}
-            <div className="flex-1 max-w-[500px] text-white">
-              <h3 className="title font-bold uppercase text-2xl">
-                this is sam's project
-              </h3>
-              <h5 className="font-normal">
-                Services: <span className="font-semibold">React + Vite</span>
-              </h5>
-            </div>
-          </div>
-          <div className="p-5 bg-green-500 project w-full md:w-[700px] min-h-[250px] max-h-[50vh] flex flex-col md:flex-row items-center justify-center gap-5">
-            {/* Red Box */}
-            <div className="w-[90vw] h-[100vh]  md:w-[400px] md:h-[300px] bg-red-300 max-w-[500px]"></div>
-
-            {/* Text */}
-            <div className="flex-1 max-w-[500px] text-white">
-              <h3 className="title font-bold uppercase text-2xl">
-                this is sam's project
-              </h3>
-              <h5 className="font-normal">
-                Services: <span className="font-semibold">React + Vite</span>
-              </h5>
-            </div>
-          </div>
-          <div className="p-5 bg-green-500 project w-full md:w-[700px] min-h-[250px] max-h-[50vh] flex flex-col md:flex-row items-center justify-center gap-5">
-            {/* Red Box */}
-            <div className="w-[90vw] h-[100vh]  md:w-[400px] md:h-[300px] bg-red-300 max-w-[500px]"></div>
-
-            {/* Text */}
-            <div className="flex-1 max-w-[500px] text-white">
-              <h3 className="title font-bold uppercase text-2xl">
-                this is sam's project
-              </h3>
-              <h5 className="font-normal">
-                Services: <span className="font-semibold">React + Vite</span>
-              </h5>
-            </div>
-          </div>
-          <div className="p-5 bg-green-500 project w-full md:w-[700px] min-h-[250px] max-h-[50vh] flex flex-col md:flex-row items-center justify-center gap-5">
-            {/* Red Box */}
-            <div className="w-[90vw] h-[100vh]  md:w-[400px] md:h-[300px] bg-red-300 max-w-[500px]"></div>
-
-            {/* Text */}
-            <div className="flex-1 max-w-[500px] text-white">
-              <h3 className="title font-bold uppercase text-2xl">
-                this is sam's project
-              </h3>
-              <h5 className="font-normal">
-                Services: <span className="font-semibold">React + Vite</span>
-              </h5>
-            </div>
-          </div>
-          <div className="p-5 bg-green-500 project w-full md:w-[700px] min-h-[250px] max-h-[50vh] flex flex-col md:flex-row items-center justify-center gap-5">
-            {/* Red Box */}
-            <div className="w-[90vw] h-[100vh]  md:w-[400px] md:h-[300px] bg-red-300 max-w-[500px]"></div>
-
-            {/* Text */}
-            <div className="flex-1 max-w-[500px] text-white">
-              <h3 className="title font-bold uppercase text-2xl">
-                this is sam's project
-              </h3>
-              <h5 className="font-normal">
-                Services: <span className="font-semibold">React + Vite</span>
-              </h5>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
