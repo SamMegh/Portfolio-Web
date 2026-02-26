7d8444d60379dffb918b0b999301687a0c414ccb
sam.megh0305@gmail.com <sam.megh0305@gmail.com>
2026-02-26 17:50:02 +0530

    website production

--- src/screens/projectScreen.jsx
+++ src/screens/projectScreen.jsx
@@ -1,9 +1,7 @@
 import gsap from "gsap";
 import { useEffect, useRef, useState } from "react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-const projects = import("../assets/data/projects.json", {
-  assert: { type: "json" }
-}).then((module) => module.default);
+import projects from "../assets/data/projects.json";
 
 gsap.registerPlugin(ScrollTrigger);
 
